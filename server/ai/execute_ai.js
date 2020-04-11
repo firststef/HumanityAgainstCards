43d69d8709baa223178a09a650ec74d6a44fd695
blox-dev <bloxorz51gaming@gmail.com>
2020-04-11 13:43:47 +0200

    Added AI function

--- server/ai/execute_ai.js
+++ server/ai/execute_ai.js
@@ -1,23 +1,56 @@
-const exec = require("child_process").execFile,
-	log = require("../utils/log"),
-	color = require("../colors"),
-	config = require("../config.js"),
-	ai_path = "C:\\Users\\nassa\\OneDrive\\Desktop\\HumanityAgainstCards\\server\\ai\\Project1.exe";
-f_header = "[routes/ai/execute_ai.js]";
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://fluffypanda:thefluffa5@humanityagainstcards-vfnzh.gcp.mongodb.net/test?retryWrites=true&w=majority";
 
-module.exports = {
-	start_ai: (params) => {
-		return new Promise((resolve, reject) => {
-			try {
-				if (!params) throw `No params provided !`;
-				exec(`${ai_path} ${params}`, function (err, data) {
-					if (err) throw `${err}`;
-					resolve(data);
-				});
-			} catch (e) {
-				console.log(log.date_now() + f_header, color.red, "Exception thrown : ", color.white, e);
-				reject(e);
-			}
-		});
-	},
-};
+function calculateFitness(relations){
+  var poz = 0;
+  var tmp = Array();
+  relations.forEach(i => {
+      tmp.push(i.value);
+  });
+  return tmp;
+}
+
+function select(wheel){
+    var pos = Math.floor(Math.random()*(wheel[wheel.length-1]));
+    for(let i =0 ; i<wheel.length ; i++){
+      if(pos<wheel[i])
+        return i;
+    }
+    return wheel.length-1;
+}
+
+function selectBest(fitness) {
+
+  var wheel = Array();
+  wheel.push(fitness[0]);
+
+  for(let i=1 ; i<fitness.length ; i++){
+    wheel.push(wheel[i-1]+fitness[i]);
+  }
+
+  return select(wheel);
+}
+
+async function getAiAnswer(black_card, white_cards){
+    var client;
+  try{
+    client = await MongoClient.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true });
+
+    var relations = await client.db("HumansAgainstCards").collection("blackcard_whitecard_relation").find({ blackCardId:black_card, whiteCardId : { $in : white_cards } }).toArray();
+  
+    var fitness = Array();
+  
+    fitness = calculateFitness(relations);
+  
+    var result = selectBest(fitness);
+    
+    console.log(white_cards[result]);
+    }
+    catch (e) {
+        console.error(e);
+    }
+    finally {
+        await client.close();
+    }
+}
+exports.getAiAnswer = getAiAnswer;
