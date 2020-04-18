efc9d5d5f60a6ae3292ee9feb6368cd5b0fc6f15
CotiugaIlie <61407980+CotiugaIlie@users.noreply.github.com>
2020-04-18 14:53:52 +0300

    Update library.js

--- client/gameclient/library.js
+++ client/gameclient/library.js
@@ -1,17 +1,6 @@
 const basedata=require('./basedata');
 const gameclient=require('./gameclient');
 const gamemanager=require('./gamemanager');
-Object.freeze(basedata.RequestHeaders);
-
-
-
-function fetchAI(blackCard, listOfCards){
-    return {
-        then: (func) => func(listOfCards[0])
-    };
-}
-
-
 
 /** Momentan reprezinta un singur joc, e treaba backend-ului sa aiba grija de apelurile catre fiecare obiect GameManager sa fie facut corect */
 
@@ -23,4 +12,5 @@ module.exports.RequestHeaders = basedata.RequestHeaders;
 module.exports.GameStates = basedata.GameStates;
 module.exports.Card = basedata.Card;
 module.exports.GameManager = gamemanager.GameManager;
+module.exports.getchAI=gamemaneger.fetchAI;
 module.exports.GameClient = gameclient.GameClient;
