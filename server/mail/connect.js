ecb0869a2a69fb21b68a0304342934080ec719a8
nassar2000 <nassarm65@gmail.com>
2020-04-22 11:13:17 +0300

    transport

--- server/mail/connect.js
+++ server/mail/connect.js
@@ -4,20 +4,23 @@ const nodemailer = require("nodemailer"),
 	color = require("../colors"),
 	f_header = "[mail/connect.js]";
 
-var transporter = nodemailer.createTransport(
-	{
-		host: "smtp.gmail.com",
-		service: "gmail",
-		auth: {
-			user: config.email.user,
-			pass: config.email.pass,
+	var transporter = nodemailer.createTransport(
+		{
+			host: "smtp.gmail.com",
+			service: "gmail",
+			auth: {
+				user: config.email.user,
+				pass: config.email.pass,
+			},
+			tls:{
+				rejectUnauthorized: false
+			}
 		},
-	},
-	// (err) => { // The callback is not beeing called for this request
-	// 	if (err) console.log(`Could not connect to the mailing service!, ${err}`);
-	// 	else console.log(log.date_now() + f_header, color.green, `Connected as ${config.email.user} ! `);
-	// },
-);
+		// (err) => { // The callback is not beeing called for this request
+		// 	if (err) console.log(`Could not connect to the mailing service!, ${err}`);
+		// 	else console.log(log.date_now() + f_header, color.green, `Connected as ${config.email.user} ! `);
+		// },
+	);
 
 
 
