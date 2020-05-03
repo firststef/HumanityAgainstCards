f5836536cafed6e5b36528a2aef6db585148546f
Ana Potoroaca <potoroacaana@gmail.com>
2020-05-03 18:34:14 +0300

    renames

--- client/registerRequest.js
+++ client/registerRequest.js
@@ -1,4 +1,4 @@
-function sendRegisterJSON(){ 
+function sendRegisterJSON(){
         let server_message = document.getElementById('register_message'); 
         let username = document.getElementById('register_username'); 
         let nickname = document.getElementById('register_nickname'); 
@@ -6,15 +6,15 @@ function sendRegisterJSON(){
         let password = document.getElementById('register_password');
     
         server_message.style.color='red';
-         console.log("Script for req");
+         console.log("Script for register request");
 
 
     // Creating a XHR object
         let xhr = new XMLHttpRequest(); 
 //TO DO route config
 
-        let url = "/server/routes/auth/register.js";
-    
+        let url = "http://localhost:8081/routes/auth/login";
+
         // open a connection 
         xhr.open("POST", url, true);
         // Set the request header i.e. which type of content you are sending
@@ -38,7 +38,8 @@ function sendRegisterJSON(){
             if (xhr.readyState == 4) {
                 if(xhr.status==200){
                     server_message.style.color='green';
-                    server_message.innerHTML='Account created succesfully';
+                    server_message.innerHTML='Succes! Go activate your account!';
+                    document.getElementById('account_activation').style.display='block';
                     //Redirect to log in page
                      window.location.href="login-register.html";
                      window.location.replace( "login-register.html" );
