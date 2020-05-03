e734021c8c73f36bf87e6a95651ed7f092410b5e
Ana Potoroaca <potoroacaana@gmail.com>
2020-05-03 19:04:41 +0300

    modifed request method

--- client/loginRequest.js
+++ client/loginRequest.js
@@ -1,51 +1,47 @@
+const sendHttpRequest2 = (method, url, data) => {
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: data ? { 'Content-Type': 'application/json' } : {}
+    }).then(response => {
+        if (response.status >= 400) {
+            // !response.ok
+            return response.json().then(errResData => {
+                const error = new Error('Something went wrong!');
+                error.data = errResData;
+                throw error;
+            });
+        }
+        return response.json();
+    });
+};
+
+
 function sendLoginJSON(){
     let server_message = document.getElementById('login_message');
     let email = document.getElementById('login_email');
     let password = document.getElementById('login_password');
 
     server_message.style.color='red';
-
-    // Creating a XHR object
-    let xhr = new XMLHttpRequest();
-//TO DO route config
-    let url = "http://localhost:8081/routes/auth/login.js";
-    // open a connection
-    xhr.open("POST", url, true);
-
-    // Set the request header i.e. which type of content you are sending
-    xhr.setRequestHeader("Content-Type", "application/json");
+    console.log("Script for login request");
+    let url = "http://localhost:8081/routes/auth/login";
 
 
-    // Converting JSON data to string
-    var data = JSON.stringify({ "email": email.value, "password": password.value });
-    // Sending data with the request
-    xhr.send(data);
+    sendHttpRequest2('POST', url, {
+        "username": email.value,
+        "password": password.value
+    })
+        .then(responseData => {
+            console.log(responseData);
+            //set cookie
+            document.cookie=response.session;
+            //redirect to account page
+            window.location.replace( "auth/client_after_auth/account.html" );
 
-    xhr.addEventListener("readystatechange", processRequest, false);
-    xhr.onreadystatechange = processRequest;
-
-    //Load the response from the server
-    //if status==200 --> response { sucess: true, session: cookie_session }
-    //else [status==401] --> repsonse { succes: false ,  reason: e}
-
-    function processRequest(e) {
-        if (xhr.readyState == 4) {
-            if(xhr.status==200){
-                var response = JSON.parse(xhr.responseText);
-                //set cookie
-                document.cookie=response.session;
-                //redirect to account page
-                window.location.replace( "auth/client_after_auth/account.html" );
-
-            }
-            else{
-                // var response = JSON.parse(xhr.responseText);
-                // server_message.innerHTML = response.reason;
-                server_message.innerHTML = 'Input not valid';
-
-            }
-        }
-
-    }
-
+        })
+        .catch(err => {
+            console.log(err, err.data);
+            //display message
+            server_message.innerHTML = 'Input not valid!';
+        });
 }
--- client/registerRequest.js
+++ client/registerRequest.js
@@ -1,3 +1,22 @@
+const sendHttpRequest1 = (method, url, data) => {
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: data ? { 'Content-Type': 'application/json' } : {}
+    }).then(response => {
+        if (response.status >= 400) {
+            // !response.ok
+            return response.json().then(errResData => {
+                const error = new Error('Something went wrong!');
+                error.data = errResData;
+                throw error;
+            });
+        }
+        return response.json();
+    });
+};
+
+
 function sendRegisterJSON(){
     let server_message = document.getElementById('register_message');
     let username = document.getElementById('register_username');
@@ -7,49 +26,26 @@ function sendRegisterJSON(){
 
     server_message.style.color='red';
     console.log("Script for register request");
-
-
-    // Creating a XHR object
-    let xhr = new XMLHttpRequest();
-//TO DO route config
-
     let url = "http://localhost:8081/routes/auth/login";
 
-    // open a connection
-    xhr.open("POST", url, true);
-    // Set the request header i.e. which type of content you are sending
-    xhr.setRequestHeader("Content-Type", "application/json");
 
-    // Converting JSON data to string
-    var data = JSON.stringify({ "username": username.value, "password": password.value, "email": email.value, "nickname": nickname.value });
-    // Sending data with the request
-    xhr.send(data);
+    sendHttpRequest1('POST', url, {
+        "username": username.value,
+        "password": password.value,
+        "email": email.value,
+        "nickname": nickname.value
+    })
+        .then(responseData => {
 
+            console.log(responseData);
+            server_message.style.color = 'green';
+            //Display message
+            server_message.innerHTML = 'Success! Go activate your account!';
+            //Show link to activation page
+            document.getElementById('account_activation').style.display = 'block';
 
-    xhr.addEventListener("readystatechange", processRequest, false);
-    xhr.onreadystatechange = processRequest;
-
-
-    //Load the response from the server
-    //if status==200 --> response { sucess: true, username : username }
-    //else [status==401] --> repsonse { succes: false ,  reason: e}
-
-    function processRequest(e) {
-        if (xhr.readyState == 4) {
-            if(xhr.status==200){
-                server_message.style.color='green';
-                server_message.innerHTML='Success! Go activate your account!';
-                document.getElementById('account_activation').style.display='block';
-
-            }
-            else{
-                // var response = JSON.parse(xhr.responseType);
-                server_message.innerHTML = JSON.parse(xhr.response).e;
-
-            }
-        }
-
-    }
-
-
+        })
+        .catch(err => {
+            console.log(err, err.data);
+        });
 }
