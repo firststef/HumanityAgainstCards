6d9c6a14f0434f74a439089fb2a6505859fe01f8
PalasanuBogdan <palasanub@gmail.com>
2020-05-07 22:08:34 +0300

    am scos fisierul keys.js din gitignore
    am uitat sa fac asta inainte sa dau commit data trecuta si am inlocuit clientid ul si clientsecret ul  cu cel al aplicatiei

---
+++ server/routes/config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+    google:{
+        clientID:'1036788677106-gsp4nptdi0ek3dmqiuu0809sfmn654ni.apps.googleusercontent.com',
+        clientSecret:'lLnBGHD6tIyhjV0NZOKMqtw6'
+    }
+}
