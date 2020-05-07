a378e6aa741e7fea172f8f48615ccdcd16befb14
PalasanuBogdan <palasanub@gmail.com>
2020-05-07 15:42:59 +0300

    Update server/config.js

--- server/config.js
+++ server/config.js
@@ -37,8 +37,32 @@ module.exports = {
 			path: "/../../../client/assets/css/lobby.css",
 			type: "text/css"
 		},
+		"/assets/css/account.css": {
+			path: "/../../../client/assets/css/account.css",
+			type: "text/css"
+		},
+		"/assets/css/forms.css": {
+			path: "/../../../client/assets/css/forms.css",
+			type: "text/css"
+		},
+		"/auth/google/assets/css/forms.css": {
+			path: "/../../../client/auth/google/assets/css/forms.css",
+			type: "text/css"
+		},
+		"/auth/google/forms.html": {
+			path: "/../../../client/auth/google/forms.html",
+			type: "text/html"
+		},
+		"/auth/google/script.js": {
+			path: "\\..\\..\\..\\client/auth/google/script.js",
+			type: "text/javascript"
+		},
 		"/assets/css/room.css": {
 			path: "/../../../client/assets/css/room.css",
+			type: "text/css"
+		},
+		"/assets/css/purecookie.css": {
+			path: "/../../../client/assets/css/purecookie.css",
 			type: "text/css"
 		},
 		"/login-register.html": {
@@ -61,12 +85,44 @@ module.exports = {
 			path: "\\..\\..\\..\\client/HomePage.html",
 			type: "text/html"
 		},
+		"/account": {
+			path: "\\..\\..\\..\\client/account.html",
+			type: "text/html"
+		},
+		"/confirm_account": {
+			path: "\\..\\..\\..\\client/confirm_account.html",
+			type: "text/html"
+		},
 		"/assets/css/home.css": {
 			path: "/../../../client/assets/css/home.css",
 			type: "text/css"
 		},
+		"/assets/js/purecookie.js": {
+			path: "\\..\\..\\..\\client/assets/js/purecookie.js",
+			type: "text/javascript"
+		},
 		"/home.js": {
 			path: "\\..\\..\\..\\client/home.js",
+			type: "text/javascript"
+		},
+		"/loginRequest.js": {
+			path: "\\..\\..\\..\\client/loginRequest.js",
+			type: "text/javascript"
+		},
+		"/registerRequest.js": {
+			path: "\\..\\..\\..\\client/registerRequest.js",
+			type: "text/javascript"
+		},
+		"/sendActivationCode.js": {
+			path: "\\..\\..\\..\\client/sendActivationCode.js",
+			type: "text/javascript"
+		},
+		"/sendResetRequest.js": {
+			path: "\\..\\..\\..\\client/sendResetRequest.js",
+			type: "text/javascript"
+		},
+		"/validate_register_input.js": {
+			path: "\\..\\..\\..\\client/validate_register_input.js",
 			type: "text/javascript"
 		},
 		"/": {
