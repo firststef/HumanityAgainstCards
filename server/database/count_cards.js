71031e0916cd9a0ff81d20ecac07adddac2fb391
stefandr99 <61435988+stefandr99@users.noreply.github.com>
2020-05-09 16:13:41 +0300

    Code quality - database

--- server/database/count_cards.js
+++ server/database/count_cards.js
@@ -1,40 +1,57 @@
-const config = require("../config"),
-	database = require("../utils/database"),
-	color = require("../colors"),
-	log = require("../utils/log"),
-	f_header = "[database/upload_cards.js]";
+const config   = require("../config"),
+      database = require("../utils/database"),
+      color    = require("../colors"),
+      log      = require("../utils/log"),
+      f_header = "[database/upload_cards.js]";
 
 module.exports = {
-	black_cards: () => {
-		new Promise((resolve, reject) => {
-			let db = database.get_db();
-
-			db.db("HumansAgainstCards")
-				.collection("black_cards")
-				.countDocuments((err, data) => {
-					if (err) {
-						console.log(log.date_now() + f_header, color.red, "Error while counting data !\n", color.white, err);
-						reject(err);
-					} else {
-						resolve(data);
-					}
-				});
-		});
-	},
-	white_cards: () => {
-		new Promise((resolve, reject) => {
+    /**
+     * Count the black cards from database
+     */
+    black_cards: () => {
+        new Promise((resolve, reject) => {
             let db = database.get_db();
-            
-			db.db("HumansAgainstCards")
-            .collection("white_cards")
-            .countDocuments((err, data) => {
-                if (err) {
-					console.log(log.date_now() + f_header, color.red, "Error while counting data !\n", color.white, err);
-					reject(err);
-                } else {
-                    resolve(data);
-                }
-            });
-		});
-	},
+
+            db.db("HumansAgainstCards")
+                .collection("black_cards")
+                .countDocuments((err, data) => {
+                    if ( err ) {
+                        console.log(log.date_now() + f_header,
+                                    color.red,
+                                    "Error while counting data !\n",
+                                    color.white,
+                                    err
+                        );
+                        reject(err);
+                    } else {
+                        resolve(data);
+                    }
+                });
+        });
+    },
+    /**
+     * Count the white cards from database
+     */
+    white_cards: () => {
+        new Promise((resolve, reject) => {
+            let db = database.get_db();
+
+            db.db("HumansAgainstCards")
+                .collection("white_cards")
+                .countDocuments((err, data) => {
+                    if ( err ) {
+                        console.log(
+                                    log.date_now() + f_header,
+                                    color.red,
+                                    "Error while counting data !\n",
+                                    color.white,
+                                    err
+                        );
+                        reject(err);
+                    } else {
+                        resolve(data);
+                    }
+                });
+        });
+    }
 };
