392837d14e3a3237ffca77844271cc5a1811eec6
iRebeca <birleanurebeca@yahoo.com>
2020-05-09 19:48:53 +0300

    username=>session in get hosted rooms

--- server/routes/room/get_hosted_rooms.js
+++ server/routes/room/get_hosted_rooms.js
@@ -1,27 +1,26 @@
-const config   = require("../../config"),
-      room     = require("../../database/room"),
-      user     = require("../../database/user"),
-      f_header = "[routes/room/get_hosted_room.js]";
+const
+    room = require("../../database/room");
 
 module.exports = function (app) {
     app.get("/get_hosted_rooms",
-            async (req, res) => {
-                try {
+        async (req, res) => {
+            try {
 
-                    let username = await user.get_user_id(req.headers.session);
-                    let rooms = await room.get_rooms_for_host(username[0].username);
+                let rooms = await room.get_rooms_for_host(req.headers.session);
 
-                    if ( rooms === false ) throw "internal error";
+                if (rooms === false) throw "internal error";
 
-                    let room_ids = Array();
+                let room_ids = Array();
 
-                    for (let key in rooms) {
+                for (let key in rooms) {
+                    if (rooms.hasOwnProperty(key)) {
                         room_ids.push(rooms[key].id);
                     }
-
-                    res.status(200).send({ success: true, rooms: room_ids });
-                } catch (e) {
-                    res.status(401).send({ success: false, reason: e });
                 }
-            });
+
+                res.status(200).send({success: true, rooms: room_ids});
+            } catch (e) {
+                res.status(401).send({success: false, reason: e});
+            }
+        });
 };
